66ba6dd60a8d2d8c86ca4f434d15b799bbd386bf
Gabriel Zanardi Nogueira <159185296+Gabriel-Zanardi@users.noreply.github.com>
2024-04-24 18:40:11 +0000

    init css and js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,26 @@
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Apr 2024
 // This file contains the JS functions for index.html
+
+const randomNumber = Math.floor(Math.random() * 6) + 1
+
+/**
+ * This function checks if thge user has selected the random number from above
+ */
+
+function myButtonClicked() {
+  // input
+  const userNumber = parseInt(document.getElementById("rng-guess").value)
+
+  //process
+  if (userNumber == randomNumber) {
+    // output
+    document.getElementById("rng-answer").innerHTML = "You have guessed the correct number"
+  }
+
+  if (userNumber != randomNumber) {
+    // output
+    document.getElementById("rng-answer").innerHTML = "You have guessed the wrong number"
+  }
+}
